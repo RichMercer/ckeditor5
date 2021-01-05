50495b7cf718e53445b9f8eff223769548d62de2
Richard Mercer <rich@richmercer.com>
2021-01-05 18:46:36 +0000

    Added image option.

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -100,11 +100,10 @@ const config = {
 		// Configure the available image resize options.
 		resizeOptions: [
 			{
-				name: 'imageResize:original',
-				label: 'Original',
-				value: null
-			},
-			{
+				name: 'imageResize:25',
+				label: '25%',
+				value: '25'
+			},{
 				name: 'imageResize:50',
 				label: '50%',
 				value: '50'
@@ -113,6 +112,11 @@ const config = {
 				name: 'imageResize:75',
 				label: '75%',
 				value: '75'
+			},
+			{
+				name: 'imageResize:original',
+				label: 'Original',
+				value: null
 			}
 		],
 
