05e63631c2f77a818936bb25ef0a0e8d7054d64f
Richard Mercer <rich@richmercer.com>
2021-01-07 22:01:32 +0000

    Small config change for video support.

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -147,6 +147,9 @@ const config = {
 			'mergeTableCells'
 		]
 	},
+	mediaEmbed: {
+		previewsInData: true
+	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
 	language: 'en'
 };
